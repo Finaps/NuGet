10d1d92c71120f220519b48fa06f06799caaf94a
Thomas Poiesz <thomas.poiesz@finaps.nl>
2020-07-16 19:31:26 +0200

    Upgrade ef core version to 3.1.6

--- Database.EF/Interfaces/IEntityFrameworkRepository.cs
+++ Database.EF/Interfaces/IEntityFrameworkRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Database.Core.Abstractions;
@@ -6,6 +7,7 @@ namespace Database.EF.Interfaces
 {
   public interface IEntityFrameworkRepository<T> : IRepository<T> where T : class
   {
+    ValueTask<T> GetByIdAsync(Guid id);
     void RemoveRange(IEnumerable<T> entities);
     void Remove(T entity);
   }

63f833a1689854325d343a5e05ae4aefd9ce836a
Thomas <thomas.poiesz@finaps.nl>
2020-09-30 16:11:22 +0200

    Add option to add cluster configurator to mongo setup

--- Database.Mongo/Connection/MongoConnection.cs
+++ Database.Mongo/Connection/MongoConnection.cs
@@ -1,4 +1,6 @@
+using System;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
 
 namespace Database.Mongo.Connection
 {
@@ -6,9 +8,15 @@ namespace Database.Mongo.Connection
   {
     public MongoClient Client { get; private set; }
     public IMongoDatabase Database { get; private set; }
-    public MongoConnection(string connectionString, string databaseName)
+    public MongoConnection(string connectionString, string databaseName, Action<ClusterBuilder> clusterConfigurator = null)
     {
-      Client = new MongoClient(connectionString);
+      var settings = MongoClientSettings.FromConnectionString(connectionString);
+      if (clusterConfigurator != null)
+      {
+        settings.ClusterConfigurator = clusterConfigurator;
+      }
+
+      Client = new MongoClient(settings);
       Database = Client.GetDatabase(databaseName);
     }
   }
